7ffba9801460e7bb35704acdedd96cbbe9a7fa78
baohoang031111 <tan.thach36@gmail.com>
2023-11-14 00:04:49 +0700

    ..............

---
+++ 5-natour/routes/toursrRoute.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const tourRouter = express.Router();
+const tourController = require('../controller/tourController');
+
+// tourRouter.route(tourController.checkID);
+
+tourRouter.param('id', tourController.checkID)
+
+// tourRouter.param('id', (req, res, next, val) => {
+//     console.log(`Tour id is: ${val}`);
+//     let id = req.params.id;
+//     if (id >= tours.length) {
+//         return res.status(404).json({
+//             status: 'fail',
+//             message: 'Invalid'
+//         })
+//     }
+//     next();
+// })
+
+
+tourRouter.route('/')
+    .get(tourController.getAllTours)
+    .post(tourController.checkbody,tourController.createTour)
+
+tourRouter.route('/:id')
+    .get(tourController.checkID)
+    .get(tourController.getTour)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour)
+
+
+module.exports = tourRouter;
